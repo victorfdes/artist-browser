eaec2a5712f1ef8f0efeb1e34c5f205cfb1122da
Victor Fernandes <4434814+victorfdes@users.noreply.github.com>
2021-06-03 01:10:12 +0530

    add base tag and build

--- src/common/components/Loader.tsx
+++ src/common/components/Loader.tsx
@@ -1,3 +1,3 @@
 export default function Loader () {
-  return (<img src="/static/loader.svg" alt="loader" aria-hidden="true" width="64" height="64" />)
+  return (<img src="static/loader.svg" alt="loader" aria-hidden="true" width="64" height="64" />)
 }
